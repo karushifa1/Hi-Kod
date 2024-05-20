0d9307a2a53e76852c8763628e74ed1e0671358e
karushifa1 <ikra38kaya@gmail.com>
2024-05-20 23:29:03 +0300

    Add files via upload

---
+++ ödev1.py
@@ -0,0 +1,36 @@
+yas=15
+yas=str(yas)
+print(yas)
+print(type(yas))
+yas=float(yas)
+print(yas)
+print(type(yas))
+
+
+yusuf=19
+fehmi=18
+sena=5
+print(yusuf==fehmi)
+print(yusuf>=fehmi)
+print(sena==5)
+print(sena<10 and yusuf>30)
+
+sayı1= int(input('bir değer girin.'))
+sayı2= int(input('bir değer girin.'))
+print( sayı1+sayı2 , sayı1-sayı2 , sayı1*sayı2 , sayı1/sayı2)
+
+
+print(input('isminiz nedir?'))
+print(input('yaşınız nedir?'))
+print(input('hangi şehirde yaşıyorsunuz?'))
+print(input('mesleğiniz nedir?'))
+
+
+atolyeismi= 'Hi-Kod Veri Bilimi Atölyesi'
+print(atolyeismi.split())
+print(atolyeismi.lower())
+print(atolyeismi.upper())
+dizi='0123456789'
+print(dizi[0:9:2])
+print(dizi[1:9:2])
+
